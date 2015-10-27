a05c869f9b7534348740db07868324bf15dbf061
Ivan Uvarov <tonk2012@gmail.com>
2015-10-27 18:19:09 -0400

    Created structure for the app with views and other stuff

---
+++ app.js
@@ -0,0 +1,29 @@
+// Modules
+var express = require("express"),
+	hbs = require("hbs"),
+	app = express();
+ 
+// Register hbs to render views 
+app.set('view engine', 'html');
+app.engine('html', hbs.__express);
+// Serve static files
+app.use(express.static('public'));
+
+
+app.get("/", function(req, res) {
+	res.render('index', {
+		"title": "Home",
+		"test": "testing this shit"
+	});
+});
+
+app.get("*", function(req, res) {
+	res.render('404', {
+		"title": "404: Not Found."
+	});
+});
+
+var server = app.listen(3000, function () {
+  var port = server.address().port;
+  console.log('Server started on port', port);
+});
