a98274bf9fb00b538db47950d971aa70c46c0063
herstli <herstli@bu.edu>
2015-12-03 11:47:52 -0500

    Implemented mongodb and twilio

--- app.js
+++ app.js
@@ -1,22 +1,57 @@
 'use strict';
 
 // Modules
-var express = require("express"),
-	hbs = require("hbs"),
-	bodyParser = require('body-parser'),
-	request = require('request'),
-	app = express();
- 
+var express = require("express");
+var	hbs = require("hbs");
+var	bodyParser = require('body-parser');
+var	request = require('request');
+var	app = express();
+// Additional modules from express generator
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+// Database
+var mongoose = require('mongoose');
+// Twilio
+var accountSid = 'AC741c25b02127a7f89bfd89398cbb7b39';
+var authToken = 'ec0331d000609e234d0f045d8d16c1a5';
+var client = require('twilio')(accountSid, authToken);
+
 // Register hbs to render views 
 app.set('view engine', 'html');
 app.engine('html', hbs.__express);
 // Serve static files
 app.use(express.static('public'));
-// instruct the app to use the `bodyParser()` middleware for all routes
+app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/views', express.static(path.join(__dirname, '/views')));
+
+// -----------------------------------------
+// MONGODB
+// -----------------------------------------
+mongoose.connect('mongodb://admin:pwd@apollo.modulusmongo.net:27017/daxU7vob');
+
+// Create schema if doesn't already exist
+var userSchema = mongoose.Schema({
+  email: String,
+  password: String,
+  cellphone: String
+});
+
+// Compile schema into model
+var User = mongoose.model('User', userSchema);
+
+/* SANITY CHECK */
+// Show all users in db in log
+User.find(function (err, users) {
+  if (err) return console.error(err);
+  console.log('ALL USERS IN DATABASE:');
+  console.log(users);
+})
 
 // -----------------------------------------
 // GET
@@ -34,6 +69,58 @@ app.get('/', function(req, res){
   	res.render('index', {
 		"page-title": "Home"
 	});
+});
+
+// Render form to register a new user
+app.post('/register-new-user', function(req, res){
+  res.render('new-user', {
+    "page-title": "Register New User"
+  });
+});
+
+// Register new user if email already not taken
+// Otherwise, render form again for user to submit
+app.post('/add-new-user', function(req, res){
+  var userEmail = req.body.userEmail;
+  var userPassword = req.body.userPassword;
+  var userCellPhone = req.body.userCellPhone;
+
+  var newuser = new User( { email: String(userEmail), password: String(userPassword), cellphone: String(userCellPhone)});
+  //console.log(newuser.email);
+
+  // Check if email already exists in database before registering
+  User.count({email: userEmail}, function (err, count) {
+    if (count <= 0) {
+      // Save in database
+      newuser.save(function (err, junk) {
+        if (err) return console.error(err);
+      });
+
+      // Send confirmation text to user provided cell number
+      client.sendMessage({
+        to: '+' + String(userCellPhone), // Any number Twilio can deliver to
+        from: '+18455354126', // A number you bought from Twilio and can use for outbound communication
+        body: 'Thanks for joining RainOrShine!' // body of the SMS message
+      }, function(err, responseData) { //this function is executed when a response is received from Twilio
+        if (!err) { // "err" is an error received during the request, if any
+          console.log(responseData.from);
+          console.log(responseData.body);
+        }
+      });
+
+      res.render('index', {
+        "page-title": "Home",
+        "reg-status": "Registration Successful!"
+      });
+    }
+    else {
+      res.render('new-user-tryagain', {
+        "page-title": "Register New User"
+      });
+    }
+  })
+  //console.log(userEmail);
+  //console.log(userPassword); 
 });
 
 // This route receives the posted form.
@@ -73,6 +160,7 @@ app.post('/location-temperature', function(req, res){
       }
 
   	  res.render('search-results', {
+        "full-addr": fullAddr,
         "search-term": userLocation,
   			"page-title": "Search Results",
         "weather-summary": currentSummary,
@@ -189,4 +277,3 @@ function getDayString(dayNumber) {
 
   return dayString;
 }
-
