d91edc4de450ca594efdb4c9900b1380dc950b62
Ivan Uvarov <tonk2012@gmail.com>
2015-12-10 23:57:48 -0500

    Added file for storing API keys

---
+++ secret_variables.js
@@ -0,0 +1,8 @@
+module.exports = {
+    twilioKeys: {
+        accountSid: "AC741c25b02127a7f89bfd89398cbb7b39",
+        authToken: "ec0331d000609e234d0f045d8d16c1a5"
+    },
+    forecastApiKey: "97b429b5400c7110f209ae571437be6b",
+    mongoConnectionUrl: "mongodb://mainuser:fr4frfsg@ds027335.mongolab.com:27335/heroku_l34smxcf"
+}
